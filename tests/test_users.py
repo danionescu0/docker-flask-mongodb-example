55d78a97f83cb398f40521256a846232b4bf9ad8
blue <neelabalan.n@outlook.com>
2021-03-05 10:37:41 +0530

    added more tests for user demo #20

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,40 +3,98 @@ import pytest, requests
 from utils import MongoDb
 
 
-@pytest.fixture
-def get_connection():
-    col = MongoDb(host='localhost')
-    col.create_connection()
-    return col
-
-
 users_host = 'http://localhost:81'
 
+@pytest.fixture
+def collection():
+    con = MongoDb(host='localhost')
+    con.create_connection()
+    con.set_collection('users')
+    yield con 
+    con.drop()
 
-# create a using using the db
-# call users service and get the data
-# check that the data is as espected
-# delete the user using the database
-def test_get_user(get_connection):
-    get_connection.upsert('users', 100,
-                          {'name': 'John', 'email': 'test@email.eu'})
+def test_get_user(collection):
+    collection.upsert(
+        100,
+        {
+            'name': 'John', 
+            'email': 'test@email.eu'
+        }
+    )
     response = requests.get(url='{0}/users/100'.format(users_host)).json()
     assert response['_id'] == 100
     assert response['email'] == 'test@email.eu'
     assert response['name'] == 'John'
-    get_connection.delete('users', 100)
 
+def test_create_user(collection):
+    requests.put(
+        url='{0}/users/101'.format(users_host),
+        data={
+            'name': 'John Doe', 
+            'email': 'johny@email.eu'
+        }
+    ).json()
 
-# create a using using the users service
-# check that the data in the db is as espected
-# delete the user using the database
-def test_create_user(get_connection):
-    get_connection.delete('users', 101)
-    requests.put(url='{0}/users/101'.format(users_host),
-                 data={'name': 'John Doe', 'email': 'johny@email.eu'}).json()
-    response = get_connection.get('users', {'_id': 101})
+    response = collection.get({'_id': 101})
     assert len(response) == 1
     assert response[0]['_id'] == 101
     assert response[0]['email'] == 'johny@email.eu'
     assert response[0]['name'] == 'John Doe'
-    get_connection.delete('users', 101)
+
+def test_update_user(collection):
+    collection.upsert(
+        100,
+        {
+            'name': 'John', 
+            'email': 'test@email.eu'
+        }
+    )
+    requests.post(
+        url='{0}/users/100'.format(users_host),
+        data={
+            'name': 'John',
+            'email': 'john@email.com'
+        }
+    ).json()
+    response = collection.get({'_id': 100})
+
+    assert response[0] == {
+        '_id': 100,
+        'name': 'John',
+        'email': 'john@email.com'
+    }
+
+def test_get_and_delete_users(collection):
+    collection.upsert(
+        100,
+        {
+            'name': 'John', 
+            'email': 'john@email.com'
+        }
+    )
+    collection.upsert(
+        101,
+        {
+            'name': 'Doe', 
+            'email': 'doe@email.com'
+        }
+    )
+    response = requests.get(url='{}/users'.format(users_host)).json()
+    # testing get request
+    assert response == [
+        {
+            'userid': 100,
+            'name': 'John', 
+            'email': 'john@email.com'
+        },
+        {
+            'userid': 101,
+            'name': 'Doe', 
+            'email': 'doe@email.com'
+        }
+    ]
+
+    requests.delete(url='{}/users/100'.format(users_host))
+    response = collection.get({'_id': 100})
+    # asserting the delete has been done
+    assert response == [] 
--- tests/utils.py
+++ tests/utils.py
@@ -9,12 +9,18 @@ class MongoDb:
     def create_connection(self):
         self.__connection = MongoClient(self.__host, 27017)[self.__dbname]
 
-    def get(self, collection: str, query: dict, limit: int = 10, offset: int = 0):
-        return list(self.__connection[collection].find(query).limit(limit).skip(offset))
+    def set_collection(self, collection):
+        self.__collection = collection
 
-    def upsert(self, collection: str, key, data: dict):
-        self.__connection[collection].update_one({'_id': key}, {'$set': data}, upsert=True)
+    def get(self, query: dict, limit: int = 10, offset: int = 0):
+        return list(self.__connection[self.__collection].find(query).limit(limit).skip(offset))
 
-    def delete(self, collection: str, key):
-        self.__connection[collection].delete_one({'_id': key})
+    def upsert(self, key, data: dict):
+        self.__connection[self.__collection].update_one({'_id': key}, {'$set': data}, upsert=True)
+
+    def delete(self, key):
+        self.__connection[self.__collection].delete_one({'_id': key})
+
+    def drop(self):
+        self.__connection[self.__collection].drop()
 
