05cfd9900c346afe3a0162f31bc9751108bf0086
Dan Ionescu <ionescu.dan84@gmail.com>
2021-07-27 17:53:11 +0300

    developing integration for jwt token into flask

--- python/geolocation_search.py
+++ python/geolocation_search.py
@@ -1,17 +1,116 @@
-import json
+import json, sys
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
+from flask_jwt import JWT, jwt_required
 from flasgger import Swagger
 from pymongo import MongoClient, GEOSPHERE
 from bson import json_util
+from werkzeug.security import safe_str_cmp
 
 
 app = Flask(__name__)
-swagger = Swagger(app)
-places = MongoClient("mongodb", 27017).demo.places
+mongo_host = "mongodb"
+if len(sys.argv) == 2:
+    mongo_host = sys.argv[1]
+places = MongoClient(mongo_host, 27017).demo.places
+
+app.config['JWT_AUTH_URL_RULE'] = '/api/auth'
+app.config['SECRET_KEY'] = 'super-secret'
+app.config["SWAGGER"] = {
+    "title": "Swagger JWT Authentiation App",
+    "uiversion": 3,
+}
+app.config['JWT_AUTH_HEADER_PREFIX'] = 'Bearer'
+
+swag = Swagger(app,
+    template={
+        "info": {
+            "title": "Swagger Basic Auth App",
+        },
+        "consumes": [
+            "application/x-www-form-urlencoded",
+        ],
+        "produces": [
+            "application/json",
+        ],
+    },
+)
+
+
+class User(object):
+    def __init__(self, user_id, username, password):
+        self.id = user_id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+
+users = [
+    User(1, 'admin', 'secret'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+def authenticate(username: str, password: str):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+
+jwt = JWT(app, authenticate, identity)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    User authenticate method.
+    ---
+    description: Authenticate user with supplied credentials.
+    parameters:
+      - name: username
+        in: formData
+        type: string
+        required: true
+      - name: password
+        in: formData
+        type: string
+        required: true
+    responses:
+      200:
+        description: User successfully logged in.
+      400:
+        description: User login failed.
+    """
+    try:
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = authenticate(username, password)
+        if not user:
+            raise Exception("User not found!")
+
+        resp = jsonify({"message": "User authenticated"})
+        resp.status_code = 200
+        access_token = jwt.jwt_encode_callback(user)
+        # add token to response headers - so SwaggerUI can use it
+        resp.headers.extend({'jwt-token': access_token})
+    except Exception as e:
+        resp = jsonify({"message": "Bad username and/or password"})
+        resp.status_code = 401
+    return resp
 
 
 @app.route("/location", methods=["POST"])
+@jwt_required()
 def new_location():
     """Add a place (name, latitude and longitude)
     ---
