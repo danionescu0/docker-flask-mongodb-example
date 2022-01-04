973d1023a54e22c2a5ed73c913c820b6551ff0c9
Dan Ionescu <dan.ionescu@inovo.ro>
2022-01-04 14:03:03 +0200

    formatted graphql users endpoint

--- python/graphql/users.py
+++ python/graphql/users.py
@@ -1,8 +1,13 @@
 import sys
 
 from pydantic import BaseModel, Field
-from ariadne import load_schema_from_path, make_executable_schema, \
-    graphql_sync, snake_case_fallback_resolvers, ObjectType
+from ariadne import (
+    load_schema_from_path,
+    make_executable_schema,
+    graphql_sync,
+    snake_case_fallback_resolvers,
+    ObjectType,
+)
 from ariadne.constants import PLAYGROUND_HTML
 from pymongo import MongoClient
 from flask import request, jsonify
@@ -30,39 +35,32 @@ def list_users_resolver(obj, info) -> dict:
     formatted = [
         {"userid": d["_id"], "name": d["name"], "email": d["email"]} for d in user_list
     ]
-    return {
-            "success": True,
-            "users": formatted
-        }
+    return {"success": True, "users": formatted}
 
 
 def upsert_user_resolver(obj, info, userid: int, email: str, name: str) -> dict:
     try:
         payload = {
             "success": True,
-            "user": {'_id': userid, 'email': email, 'name': name}
+            "user": {"_id": userid, "email": email, "name": name},
         }
-        users.update_one({'_id': userid}, {'$set': {'email': email, 'name': name}}, upsert=True)
+        users.update_one(
+            {"_id": userid}, {"$set": {"email": email, "name": name}}, upsert=True
+        )
     except ValueError:  # date format errors
-        payload = {
-            "success": False,
-            "errors": ["errors"]
-        }
+        payload = {"success": False, "errors": ["errors"]}
     return payload
 
 
 def get_user_resolver(obj, info, userid) -> dict:
     try:
         user_data = users.find_one({"_id": userid})
-        payload = {
-            "success": True,
-            "user": user_data
-        }
+        payload = {"success": True, "user": user_data}
 
     except AttributeError:  # todo not found
         payload = {
             "success": False,
-            "errors": [f"Todo item matching id {id} not found"]
+            "errors": [f"Todo item matching id {id} not found"],
         }
     return payload
 
@@ -71,15 +69,9 @@ def delete_user_resolver(obj, info, userid: int) -> dict:
     try:
         user = users.find_one({"_id": userid})
         users.delete_one({"_id": userid})
-        payload = {
-            "success": True,
-            "user": user
-        }
+        payload = {"success": True, "user": user}
     except AttributeError:
-        payload = {
-            "success": False,
-            "errors": ["Not found"]
-        }
+        payload = {"success": False, "errors": ["Not found"]}
     return payload
 
 
@@ -90,7 +82,6 @@ query.set_field("listUsers", list_users_resolver)
 query.set_field("getUser", get_user_resolver)
 mutation.set_field("upsertUser", upsert_user_resolver)
 mutation.set_field("deleteUser", delete_user_resolver)
-
 
 
 type_defs = load_schema_from_path("graphql/schema.graphql")
@@ -107,12 +98,7 @@ def graphql_playground():
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
     data = request.get_json()
-    success, result = graphql_sync(
-        schema,
-        data,
-        context_value=request,
-        debug=app.debug
-    )
+    success, result = graphql_sync(schema, data, context_value=request, debug=app.debug)
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
